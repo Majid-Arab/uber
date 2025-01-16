9c67f237b7070f78de3f4d9c6a07fc665e1846d2
Abdul-Majid <arabmajid12@gmail.com>
2025-01-16 16:00:19 +0500

    confirm ride
    fetching drivers
    pass selected driver

--- app/(root)/confirm-ride.tsx
+++ app/(root)/confirm-ride.tsx
@@ -1,28 +1,32 @@
-import { router } from "expo-router";
-import { FlatList, View } from "react-native";
-
 import CustomButton from "@/components/CustomButton";
 import DriverCard from "@/components/DriverCard";
 import RideLayout from "@/components/RideLayout";
+import { useFetch } from "@/lib/fetch";
 import { useDriverStore } from "@/store";
+import { MarkerData } from "@/types/type";
+import { BottomSheetFlatList } from "@gorhom/bottom-sheet";
+import { router } from "expo-router";
+import { Text, View } from "react-native";
 
 const ConfirmRide = () => {
-  const { drivers, selectedDriver, setSelectedDriver } = useDriverStore();
+  const { selectedDriver, setSelectedDriver, setDrivers } = useDriverStore();
+  const { data: drivers, loading } = useFetch<MarkerData[]>(`/(api)/driver`);
 
+  if (loading) return <Text>Loading...</Text>;
   return (
-    <RideLayout title={"Choose a Rider"} snapPoints={["65%", "85%"]}>
-      <FlatList
+    <RideLayout title="Confirm Ride" snapPoints={["55%", "85%"]}>
+      <BottomSheetFlatList
         data={drivers}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item, index }) => (
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
           <DriverCard
             item={item}
-            selected={selectedDriver!}
-            setSelected={() => setSelectedDriver(item.id!)}
+            selected={selectedDriver === item}
+            setSelected={() => setSelectedDriver(item)}
           />
         )}
         ListFooterComponent={() => (
-          <View className="mx-5 mt-10">
+          <View>
             <CustomButton
               title="Select Ride"
               onPress={() => router.push("/(root)/book-ride")}
