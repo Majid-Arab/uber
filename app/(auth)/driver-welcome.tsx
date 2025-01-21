3ea8bd1d52a92d64243354acfa6c085e73491af6
Abdul-Majid <arabmajid12@gmail.com>
2025-01-21 16:46:28 +0500

    updated driver home route

--- app/(auth)/driver-welcome.tsx
+++ app/(auth)/driver-welcome.tsx
@@ -13,7 +13,7 @@ const DriverOnboarding = () => {
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
         onPress={() => {
-          router.replace("/(root)/(tabs)/driverHome");
+          router.replace("/(root)/(driverTabs)/driverHome");
         }}
         className="w-full flex justify-end items-end p-5"
       >
