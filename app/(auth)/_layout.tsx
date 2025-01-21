9b92af1c5389bf422fb95e52111d630323919e7f
Abdul-Majid <arabmajid12@gmail.com>
2025-01-21 16:46:02 +0500

    removed driver form and driver welcome header

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -4,6 +4,8 @@ const Layout = () => {
   return (
     <Stack>
       <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="driver-form" options={{ headerShown: false }} />
+      <Stack.Screen name="driver-welcome" options={{ headerShown: false }} />
       <Stack.Screen name="sign-up" options={{ headerShown: false }} />
       <Stack.Screen name="sign-in" options={{ headerShown: false }} />
     </Stack>
